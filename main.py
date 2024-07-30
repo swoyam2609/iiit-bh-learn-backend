a9a4f35656c060a1451b2e5f90f96832c4696a98
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-07-30 18:37:57 +0530

    play content api added

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from routers import user, file
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(file.router)
--- routers/file.py
+++ routers/file.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from dependencies import pass_jwt, mongo
 import os
+from pathlib import Path
 
 router = APIRouter()
 
-@router.get("/browse")
-def browseCourses(username: str = Depends(pass_jwt.get_current_user)):
+@router.get("/browse", tags=["Course"])
+async def browseCourses(username: str = Depends(pass_jwt.get_current_user)):
     user = mongo.db.users.find_one({"username": username})
     if user is None:
         return JSONResponse(content={"message":"unauthenticated"})
@@ -14,12 +15,22 @@ def browseCourses(username: str = Depends(pass_jwt.get_current_user)):
         courses = os.listdir("./files")
         return JSONResponse(content={"courses": courses})
     
-@router.get("/course")
-def getCourse(coursename: str, username: str = Depends(pass_jwt.get_current_user)):
+@router.get("/course", tags=["Course"])
+async def getCourse(courseName: str, username: str = Depends(pass_jwt.get_current_user)):
     user = mongo.db.users.find_one({"username": username})
     if user is None:
         return JSONResponse(content={"message":"unauthenticated"})
     else:
-        content = os.listdir(f"./files/{coursename}")
+        content = os.listdir(f"./files/{courseName}")
         content.sort()
         return JSONResponse(content={"courses": content})
+    
+@router.get("/play", tags=["Course"])
+async def playCourse(courseName: str, fileName: str, username: str = Depends(pass_jwt.get_current_user)):
+    user = mongo.db.users.find_one({"username": username})
+    if user is None:
+        return JSONResponse(content={"message":"unauthenticated"})
+    else:
+        videoPath = Path(f"./files/{courseName}/{fileName}")
+        videoFile = open(videoPath, mode="rb")
+        return StreamingResponse(videoFile, media_type="video/mp4")
