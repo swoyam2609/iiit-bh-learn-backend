31f9e76f53e021563e2bd7a3cd1eab5469180c63
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-07-30 18:51:27 +0530

    upload course path complete

--- routers/file.py
+++ routers/file.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from fastapi.responses import JSONResponse, StreamingResponse
 from dependencies import pass_jwt, mongo
 import os
 from pathlib import Path
+import zipfile
+import aiofiles
 
 router = APIRouter()
 
@@ -34,3 +36,22 @@ async def playCourse(courseName: str, fileName: str, username: str = Depends(pas
         videoPath = Path(f"./files/{courseName}/{fileName}")
         videoFile = open(videoPath, mode="rb")
         return StreamingResponse(videoFile, media_type="video/mp4")
+    
+@router.post("/uploadcourse", tags=["Course"])
+async def uploadCourse(courseName: str, courseFile: UploadFile = File(...)):
+    filePath = f"./files/temp/{courseFile.filename}"
+    async with aiofiles.open(filePath, "wb") as f:
+        content = await courseFile.read()
+        await f.write(content)
+
+    coursePath = f"./files/{courseName}"
+    os.makedirs(coursePath, exist_ok=True)  # Ensure the directory exists
+
+    # Extract the ZIP file
+    with zipfile.ZipFile(filePath, 'r') as zipRef:
+        zipRef.extractall(coursePath)
+
+    # Remove the ZIP file
+    os.remove(filePath)
+
+    return JSONResponse(content={"message": "course uploaded"})
