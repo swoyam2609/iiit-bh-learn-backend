b4d1c386e2ce835c8c4014ded1a8ba1fcba09fec
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-07-30 18:27:07 +0530

    course content api complete

--- routers/file.py
+++ routers/file.py
@@ -13,3 +13,13 @@ def browseCourses(username: str = Depends(pass_jwt.get_current_user)):
     else:
         courses = os.listdir("./files")
         return JSONResponse(content={"courses": courses})
+    
+@router.get("/course")
+def getCourse(coursename: str, username: str = Depends(pass_jwt.get_current_user)):
+    user = mongo.db.users.find_one({"username": username})
+    if user is None:
+        return JSONResponse(content={"message":"unauthenticated"})
+    else:
+        content = os.listdir(f"./files/{coursename}")
+        content.sort()
+        return JSONResponse(content={"courses": content})
